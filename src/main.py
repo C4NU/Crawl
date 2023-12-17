bf53c1cbfca6b63618e946727091b474862f8d2c
C4NU <canu1832@gmail.com>
2023-12-17 22:00:50 +0900

    인터벌 추가, 차이값 추가

--- src/main.py
+++ src/main.py
@@ -9,9 +9,7 @@ import asyncio
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-dataset = crawl.USDC_Cool()
+value_data = dict()
 
 def get_xpath_data(dom, string):
 	return dom.xpath(string)[0].text
@@ -24,20 +22,48 @@ def human_format(num):
 		num /= 1000.0
 	return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
 
-async def main():
+def calc_difference(value_old, value_new):
+	if value_old is not None:
+			change = value_new - value_old
+			rate = (change / value_old) * 100 if value_old != 0 else 0
+			return f'{rate:.2f}'
+	else:
+		return '0.00%'
+
+async def send_data():
+	text = time.strftime('%Y-%m-%d %H:%M:%S\n')
+	for coin in dataset.xpath_dict.keys():
+		try:
+			value_data[coin][0] = value_data[coin][1]
+		except:
+			value_data[coin] = [0,0]
+
+		value = get_xpath_data(dataset.dom, dataset.xpath_dict[coin])
+		value = value.replace(',', '')
+		value_data[coin][1] = int(value)
+
+		difference = calc_difference(value_data[coin][0], value_data[coin][1])
+
+		value_format = human_format(value_data[coin][1])
+		text = text + (coin + " | " + value_format + " USDC Issued, " + difference + "% \n")
+
+	await bot.sendMessage(chat_id=chat_id, text=text)
+
+async def task(interval_time, func):
+	while True:
+		await func()
+		await asyncio.sleep(interval_time * 3600)
+
+if __name__ == '__main__':
+	load_dotenv()
+
+	dataset = crawl.USDC_Cool()
 	token = os.environ.get('token')
 	chat_id = os.environ.get('chat_id_personal')
 
 	bot = telegram.Bot(token=token)
 
-	text = time.strftime('%Y-%m-%d %H:%M:%S\n')
-	for coin in dataset.xpath_dict.keys():
-		value = get_xpath_data(dataset.dom, dataset.xpath_dict[coin])
-		value = value.replace(',', '')
-		print(value)
-		value = int(value)
-		value = human_format(value)
-		text = text + (coin + " | " + value + " USDC Issued\n")
-	await bot.sendMessage(chat_id=chat_id, text=text)
+	interval_time = 2
 
-asyncio.run(main())
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(task(interval_time, send_data))
